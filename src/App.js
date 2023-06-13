e6cd61c142eb1d69c3c69509be393177eda0c882
MariaEduardaSM <mariadudamiguel28@gmail.com>
2023-06-12 21:14:48 -0300

    aula 12/06/2023

--- src/App.js
+++ src/App.js
@@ -1,51 +1,18 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
-
-import Simples from "./componentes/Simples";
-import Frag from "./componentes/Frag";
-import ParImpar from "./componentes/ParImpar";
-import Familia from "./componentes/relacao/Familia";
-import Membro from "./componentes/relacao/Membro";
-import UsuarioLogado from "./componentes/UsuarioLogado";
-import Botao from "./componentes/Botao";
-import Contador from "./componentes/Contador";
-import DigiteSeuNome from "./componentes/DigiteSeuNome";
-import DimensoesFixas from "./componentes/DimensoesFixas";
-import Mega from "./componentes/MegaSena/Mega";
-import Calculadora from "./componentes/Calculadora/Calculadora";
+import { NavigationContainer } from '@react-navigation/native';
+import {MainStackNavigator} from "./componentes/Menu/StackNavigator";
 
 export default class App extends Component {
   render() {
-    return (
-
-      <View style={estilos.container}>
-
-        {/*<Simples texto='Duda' /> 
-         <Frag titulo="Cadastro" 
-              subTitulo="Tela de Cadastro de Produto" /> 
-         <ParImpar numero={9}  /> 8 
-        <Familia>
-            <Membro nome="julia" sobreNome="Rodrigues"/>
-            <Membro nome="rafael" sobreNome="Rodrigues"/> 
-        </Familia> 
-        <UsuarioLogado usuario={ {nome:'teste', email:'teste@teste.com'} } /> 
-        <UsuarioLogado usuario={ {nome:'teste 2'} } />
-        <UsuarioLogado usuario={ {email:'teste@teste.com'} } />        
-        <UsuarioLogado usuario={ {} } />
-        <UsuarioLogado usuario={ null } /> 
-        <Contador valorInicial={50} />         
-        <DigiteSeuNome /> 
-        <DimensoesFixas /> 
-        <Mega qtdeNumeros={7} />  */}
-
-        <Calculadora />
-
-      </View>
-
+    return(
+    <NavigationContainer>
+      <MainStackNavigator />
+    </NavigationContainer>
     );
-  }
+  }   
 }
-
+  
 const estilos = StyleSheet.create({
   container: {
     flex: 1,
---
+++ src/componentes/Menu/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {View,Button}from "react-native";
+import Padrao from "../../estilo/Padrao";
+
+const Home = ({navigation}) => { 
+    return (
+        <View style={Padrao.center}>
+        <Button
+           title="ir para a tela about"
+           onPress={() => navigation.navigate("About")}
+         />
+
+        </View>
+    );
+};
+
+export default Home
+
+
+
+
+
+
